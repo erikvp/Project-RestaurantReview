d15b35c2194a866d73f3d00602f8fce7343a666a
erikvp <erikvp@yahoo.com>
2018-08-06 16:06:59 -0400

    service worker revisions - refresh not working

--- js/sw.js
+++ js/sw.js
@@ -1,11 +1,10 @@
 /* References for the code below:
- * https: //github.com/Sugahzor/udacity-restaurants-stage1/blob/master/sw.js
- * https: //github.com/stearruda/restaurant-reviews-app-stage-1/blob/master/sw.js
+ * https: //developers.google.com/web/fundamentals/primers/service-workers/
  */
 
 
-let staticCacheName = 'restaurant-static-v1';
-let urlCache = [
+let CACHE_NAME = 'restaurant-cache-v1';
+let urlsToCache = [
   '/',
   '/index.html',
   '/restaurant.html',
@@ -24,47 +23,48 @@ let urlCache = [
   '/img/7.jpg',
   '/img/8.jpg',
   '/img/9.jpg',
-  '/img/10.jpg',
-  // '/img/11.jpg' //for testing error
+  '/img/10.jpg'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      return cache.addAll(urlCache).then(() => {
-        console.log('PASS: All files cached');
-      }).catch((error) => {
-        console.log(`FAIL: Caching error: ${error}`);
-      })
-    })
-  ); //waitUntil
-}); //eventListener
+self.addEventListener('install', function (event) {
+  // Perform install steps
+  event.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
+    console.log('Opened cache');
+    return cache.addAll(urlsToCache);
+  }));
+});
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys()
-    .then((cacheNames) => {
-      return Promise.all(
-        cacheNames.filter((cacheName) => {
-          return cacheName.startsWith('restaurant-') &&
-            cacheName != staticCacheName;
-        }).map(function (cacheName) {
-          return caches.delete(cacheName);
-        })
-      ); //Promise.all
-    })
-  ); //waitUntil
-}) //eventListener
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    // ignore query (?id=) at end of url: http://localhost:8000/restaurant.html?id=2
-    caches.match(event.request, {
-      ignoreSearch: true
-    })
-    .then((response) => {
-      return response || fetch(event.request);
-    })
-    .catch((err) => console.log(err, event.request))
-  );
-}); //eventListener
+self.addEventListener('fetch', function (event) {
+  event.respondWithcaches.match(event.request, {
+    ignoreSearch: true
+  }).then(function (response) {
+    // Cache hit - return response
+    if (response) {
+      return response;
+    }
+    var fetchRequest = event.request.clone();
+    return fetch(fetchRequest).then(function (response) {
+      // Check if we received a valid response
+      if (!response || response.status !== 200 || response.type !== 'basic') {
+        return response;
+      }
+      var responseToCache = response.clone();
+      caches.open(CACHE_NAME).then(function (cache) {
+        cache.put(event.request, responseToCache);
+      });
+      return response;
+    });
+  })
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(caches.keys().then(function (cacheNames) {
+    return Promise.all(cacheNames.map(function (cache) {
+      if (cache !== CACHE_NAME) {
+        console.log(`remove files from ${cache}`);
+        return caches.delete(cache);
+      }
+    }));
+  }));
+});
