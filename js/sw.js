8ac745368a84a288db2e330b050cef084c84cd60
erikvp <erikvp@yahoo.com>
2018-08-06 17:21:45 -0400

    service worker revisions

--- js/sw.js
+++ js/sw.js
@@ -2,7 +2,6 @@
  * https: //developers.google.com/web/fundamentals/primers/service-workers/
  */
 
-
 let CACHE_NAME = 'restaurant-cache-v1';
 let urlsToCache = [
   '/',
@@ -36,27 +35,37 @@ self.addEventListener('install', function (event) {
 
 
 self.addEventListener('fetch', function (event) {
-  event.respondWithcaches.match(event.request, {
-    ignoreSearch: true
-  }).then(function (response) {
-    // Cache hit - return response
-    if (response) {
-      return response;
-    }
-    var fetchRequest = event.request.clone();
-    return fetch(fetchRequest).then(function (response) {
-      // Check if we received a valid response
-      if (!response || response.status !== 200 || response.type !== 'basic') {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function (response) {
+      // Cache hit - return response
+      if (response) {
         return response;
       }
-      var responseToCache = response.clone();
-      caches.open(CACHE_NAME).then(function (cache) {
-        cache.put(event.request, responseToCache);
-      });
-      return response;
-    });
-  })
+
+      var fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then(
+        function (response) {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          var responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(function (cache) {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+    })
+  );
 });
+
+
 
 self.addEventListener('activate', function (event) {
   event.waitUntil(caches.keys().then(function (cacheNames) {
